47d2cfd5c4ac131b433777519ca85e03fbd899b7
Ubaikab <ubaikabshaikh@gmail.com>
2024-09-18 13:05:52 +0530

    first commit

--- Assignment-1/script.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navList = document.querySelector('.nav-list');
-
-menuToggle.addEventListener('click', () => {
-  navList.classList.toggle('active');
-  if (!navList.classList.contains('active')) {
-    navList.style.animation = 'none';
-  }
-});
